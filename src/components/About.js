912413bbfc7495e316afe71a5ceb1b7a197398ca
Eunji (Silvia) Kwon <sandeul97@gmail.com>
2024-08-13 15:43:08 +0000

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
-import '../CustomStyles.css'; // CustomStyles.css 파일 import
+import '../../CustomStyles.css'; // CustomStyles.css 파일 import
 import '../index.css';
 
 const About = () => {
