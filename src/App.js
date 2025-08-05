956106fb9fd597001329f2648ecf3942b55b660a
Eunji Kwon <sandeul97@gmail.com>
2025-08-05 17:46:56 -0400

    nav bar fix/hidden 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Element } from 'react-scroll'; // For scroll functionality
@@ -13,10 +13,13 @@ import Footer from './components/partials/Footer';
 import './App.css';
 
 // 새 네비게이션 바 컴포넌트 추가
-function Navbar() {
+function Navbar({visible}) {
   return (
-    <nav className="navbar navbar-expand navbar-light bg-light justify-content-center my-3">
-      <ul className="navbar-nav flex-row">
+    <nav
+      className={`navbar navbar-expand navbar-light bg-light justify-content-center ${visible ? 'navbar--visible' : 'navbar--hidden'}`}
+      onMouseEnter={() => { /* handled in parent */ }}
+      onMouseLeave={() => { /* handled in parent */ }}
+    >      <ul className="navbar-nav flex-row">
         <li className="nav-item mx-2">
           <a className="nav-link" href="#about">About</a>
         </li>
@@ -35,12 +38,38 @@ function Navbar() {
 }
 
 function App() {
+
+  const [navbarVisible, setNavbarVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 80) {
+        setNavbarVisible(false);
+      } else {
+        setNavbarVisible(true);
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // 마우스가 상단 60px에 들어오면 네비바 보이기
+    const handleMouseMove = (e) => {
+      if (e.clientY < 60) setNavbarVisible(true);
+    };
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <Router>
-      {/* <div className="sidebar">
-        <Header />
-      </div> */}
 
+          <Navbar visible={navbarVisible}/> {/* 네비게이션 바 추가 */}
       <div className="app-content">
         <Routes>
           {/* Route for the main page */}
@@ -48,7 +77,6 @@ function App() {
             path="/"
             element={
               <>
-                                <Navbar /> {/* 네비게이션 바 추가 */}
                 <Element name="about">
                   <About />
                 </Element>
